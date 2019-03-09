8131b3616d9ebf9a181167799aec114b41d57850
Sing <sing@paddingleft.com>
2019-03-09 14:34:05 +0000

    Fix travis build script [ci skip]

---
+++ precache-manifest.fe5fe6d1b0861ecfb2030f26ede3d4ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d3ac307c8fc2054b7f4",
+    "url": "junk-encoder/static/js/runtime~main.47706080.js"
+  },
+  {
+    "revision": "c70692b1b79058dd00d7",
+    "url": "junk-encoder/static/js/main.362bb87a.chunk.js"
+  },
+  {
+    "revision": "b7b02114fa92689289ec",
+    "url": "junk-encoder/static/js/2.442268d3.chunk.js"
+  },
+  {
+    "revision": "c70692b1b79058dd00d7",
+    "url": "junk-encoder/static/css/main.c9e33269.chunk.css"
+  },
+  {
+    "revision": "b7b02114fa92689289ec",
+    "url": "junk-encoder/static/css/2.bbf46a9d.chunk.css"
+  },
+  {
+    "revision": "b00bd8f8679889703003707fe2407ce2",
+    "url": "junk-encoder/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/junk-encoder/build/precache-manifest.d21c1ef6c952793db68c228d258d18db.js"
+  "junk-encoder/precache-manifest.fe5fe6d1b0861ecfb2030f26ede3d4ae.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/junk-encoder/build/index.html", {
+workbox.routing.registerNavigationRoute("junk-encoder/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
