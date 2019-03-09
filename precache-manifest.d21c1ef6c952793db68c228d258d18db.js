74c222ce81a3fbe56a914e2863ce38b06e239cd3
Sing <sing@paddingleft.com>
2019-03-09 14:26:07 +0000

    Fix travis build script [ci skip]

---
+++ precache-manifest.d21c1ef6c952793db68c228d258d18db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "43794beff32b02d03288",
+    "url": "/junk-encoder/build/static/js/runtime~main.0cd998f6.js"
+  },
+  {
+    "revision": "e9d4abb0474e58ac3869",
+    "url": "/junk-encoder/build/static/js/main.5fd74c50.chunk.js"
+  },
+  {
+    "revision": "b7b02114fa92689289ec",
+    "url": "/junk-encoder/build/static/js/2.442268d3.chunk.js"
+  },
+  {
+    "revision": "e9d4abb0474e58ac3869",
+    "url": "/junk-encoder/build/static/css/main.c9e33269.chunk.css"
+  },
+  {
+    "revision": "b7b02114fa92689289ec",
+    "url": "/junk-encoder/build/static/css/2.bbf46a9d.chunk.css"
+  },
+  {
+    "revision": "b664935f6923ed1814bb301d051b4480",
+    "url": "/junk-encoder/build/index.html"
+  }
+];
